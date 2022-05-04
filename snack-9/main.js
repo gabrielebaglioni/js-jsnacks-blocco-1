dca6f684ba79f9ae2b1951a8d587c23a4525645f
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 21:13:19 +0200

    finito

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,7 +1,7 @@
 // creare due array con numero elementi diversi
 // creare un ciclo for che aggiunge gli elemeti al più piccolo
-const array1 = [1, 2, 3, 4,]
-const array2 = [1, 2]
+const array1 = [1] //[1, 2, 3, 4, 5, 6, 7, 8,]
+const array2 = [1, 2, 3]
 // definisco una variabile da stampare con la differenza delle lunghezze 
 let differenza = 0
  if (array1.length > array2.length){
@@ -15,7 +15,10 @@ let differenza = 0
  else if ( array1.length < array2.length){
     const differenza = array2.length - array1.length;
     console.log(differenza);
-    array2.push(Math.floor(Math.random()*100) + 1);
+    for(let i = 0 ; i < differenza; i++){
+      array1.push(Math.floor(Math.random()*100) + 1);
+    }
+    
  }
 console.log(array1);
 console.log(array2);
