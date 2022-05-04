160baa8e9f243bab8b82abc1b4048d0ef3a87547
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 12:46:26 +0200

    finito

--- snack-5/main.js
+++ snack-5/main.js
@@ -5,7 +5,7 @@ const numeridispari = [];
 for( let i=0 ; i < 6 ; i++){
    let numero = Number(prompt("inserisci un numero"));
    if (numero % 2 !== 0){
-      
+      numeridispari.push(numero);
    }
-
 }
+console.log(numeridispari)
