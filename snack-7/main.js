767551cab85a13bb62d9ab9ef460d9dc9fb08dea
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 17:55:05 +0200

    implementata la somma

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,5 +1,12 @@
-const array1 = ["gabriele", "tommaso", "giorgio"];
-const array2= ["baglioni", "marino", "carratta"];
-const listavuota = []
-const array3 = array1.concat(array2);
- console.log(array3);
+const nomi = ["gabriele", "tommaso", "giorgio"];
+const cognomi= ["baglioni", "marino", "carratta"];
+ let listavuota = [];
+ for (let i = 0; i < 3; i++) {
+    const iNomi = Math.floor(Math.random() * nomi.length);
+    const iCognomi = Math.floor(Math.random() * cognomi.length);
+
+    listavuota.push(`${nomi[iNomi]} ${cognomi[iCognomi]}`);
+ }
+ console.log(listavuota)
+// const array3 = array1.concat(array2);
+//  console.log(array3);
--- snack-8/main.js
+++ snack-8/main.js
@@ -1,11 +1,11 @@
 //creare un array di numeri interi
 //selezionare gli elementi in posizione dispari
 //sommare gli elementi selezionati
-const numeriInteri = ["1","2","3","4,"];
-const somma = [];
+const numeriInteri = ["1","2","3","4",];
+let somma = [];
 for (let i = 0; i < numeriInteri.length; i++){
-   if(i % 2 !===0){
-      
-   }
+    if(i%2===1)
+    somma += numeriInteri[i];
 }
+console.log(somma);
 
