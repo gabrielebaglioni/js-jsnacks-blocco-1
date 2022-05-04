b6b25cef82ad6780b45fde882f88664887c1d031
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 11:52:51 +0200

    aggiunto else

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,6 +4,9 @@ const parola2 = prompt("inserisci una parola");
 if(parola1.length > parola2.length){
    console.log(parola1);
 }
-else{
+else if (parola1.length < parola2.length){
    console.log(parola2)
+}
+else{
+   console.log("sono lunghe uguali")
 }
