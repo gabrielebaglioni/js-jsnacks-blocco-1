09ac2996e5a9d78a56641e6a017ea469f2e7a91b
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 11:35:26 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,9 @@
+const number1 = Number(prompt("inserisci un numero"));
+const number2 = Number(prompt("inserisci un numero"));
+ if (number1 > number2){
+   console.log(number1);
+ }
+
+ else{
+   console.log(number2);
+ }
