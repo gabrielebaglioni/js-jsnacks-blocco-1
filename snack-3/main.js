81da279047c8e77d6f50b70d95f0538c7de786e7
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 12:10:58 +0200

    finito

---
+++ snack-3/main.js
@@ -0,0 +1,8 @@
+
+// console.log("la somma è di : ");
+let somma = 0;
+for( let i=0 ; i < 10 ; i++){
+   let numero = Number(prompt("inserisci un numero"));
+   somma += numero;
+}
+console.log("la somma di tutti inumeri è...:", somma)
