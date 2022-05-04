e2cfde83e288f9950df0ba1f8647b871a275f6e7
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 17:44:27 +0200

    logica fatta

---
+++ snack-8/main.js
@@ -0,0 +1,11 @@
+//creare un array di numeri interi
+//selezionare gli elementi in posizione dispari
+//sommare gli elementi selezionati
+const numeriInteri = ["1","2","3","4,"];
+const somma = [];
+for (let i = 0; i < numeriInteri.length; i++){
+   if(i % 2 !===0){
+      
+   }
+}
+
