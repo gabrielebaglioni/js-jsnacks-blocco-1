c7a2a98aab5c656bc06545f5b0cae6ea740911de
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 12:40:11 +0200

    logica numero dispri

---
+++ snack-5/main.js
@@ -0,0 +1,11 @@
+//creo un array vuoto 
+//chiedo per 6 volte automaticamente un numero all'utente
+//se è dispari lo inserisco nell'array
+const numeridispari = [];
+for( let i=0 ; i < 6 ; i++){
+   let numero = Number(prompt("inserisci un numero"));
+   if (numero % 2 !== 0){
+      
+   }
+
+}
