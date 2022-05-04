b2362690b2e68f2a339ea5b9215bbf56f1983b08
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 19:39:11 +0200

    finto

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,6 +1,6 @@
 // creare due array con numero elementi diversi
 // creare un ciclo for che aggiunge gli elemeti al più piccolo
-const array1 = [1,]
+const array1 = [1, 2, 3, 4,]
 const array2 = [1, 2]
 // definisco una costante che faccia la differenza tra le lunghezze dei due array per capire 
 //se è maggiore o uguale a 0
@@ -8,13 +8,17 @@ let differenza = 0
  if (array1.length > array2.length){
    const differenza =array1.length - array2.length;
    console.log(differenza);
+   for(let i = 0 ; i < differenza; i++){
+      array2.push(Math.floor(Math.random()*100) + 1);
+      
+   }
  }
  else if ( array1.length < array2.length){
     const differenza = array2.length - array1.length;
     console.log(differenza);
+    array2.push(Math.floor(Math.random()*100) + 1);
  }
-//  let differenza= corto.length - lungo.length;
+console.log(array1);
+console.log(array2);
  //se la differenza è maggiore di zero allora creao un ciclo for per sommare gli elementi
-//  if (differenza>0){
-//     for(let i = 0;)
-//  }
+ //inserire il numero della differenza ag
