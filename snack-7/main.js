eee280da3041dadd40e79692145ac3f3d923b565
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-04 16:59:00 +0200

    prova

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,4 +1,5 @@
 const array1 = ["gabriele", "tommaso", "giorgio"];
 const array2= ["baglioni", "marino", "carratta"];
+const listavuota = []
 const array3 = array1.concat(array2);
  console.log(array3);
